ff14b45c0a816837fe7927c144144a8cefed0d5a
AntonioAndradeGomes <gomesmax1997@gmail.com>
2026-07-19 21:46:10 -0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center" width="100%">
+    
+</p>
+
+<h3 align="center">
+    Desafio Backend do PicPay
+</h3>
--- src/main/java/com/example/picpay/PicpayApplication.java
+++ src/main/java/com/example/picpay/PicpayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class PicpayApplication {
-
+	//https://youtu.be/dttXo48oXt4?list=PLxCh3SsamNs7y1Y-QaVdWx0MUh0wvo7TV
 	public static void main(String[] args) {
 		SpringApplication.run(PicpayApplication.class, args);
 	}
